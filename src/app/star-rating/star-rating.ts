f27bae888ad071e1273a4917685d5a6b7ab3c4dd
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2025-07-14 12:57:02 -0300

    fix/star-rating: fix lint error in setStars

--- src/app/star-rating/star-rating.ts
+++ src/app/star-rating/star-rating.ts
@@ -1,13 +1,4 @@
-// Improved version
-
-import {
-  Component,
-  computed,
-  inject,
-  input,
-  output,
-  signal,
-} from '@angular/core';
+import { Component, computed, inject, input, output, signal } from '@angular/core';
 import { NarrationService } from '../narration-service';
 
 function range(from: number, to: number) {
@@ -59,7 +50,7 @@ export class StarRatingComponent {
   starInfo = computed<Array<StarInfo>>(() => {
     const cur = this.currentRating();
     const max = this.maxRating();
-    const info = range(0, max).map((idx) => ({
+    const info = range(0, max).map(idx => ({
       which: idx + 1, // map (0..max - 1) to (1..max)
       filled: idx < cur,
     }));
@@ -67,19 +58,18 @@ export class StarRatingComponent {
   });
 
   protected setStars(numStars: number) {
-    numStars = this.normalizeStars(numStars);
-    if (numStars === this.currentRating()) {
+    const numStarsBounded = this.normalizeStars(numStars);
+    if (numStarsBounded === this.currentRating()) {
       return; // unchanged
     }
 
-    this.currentRating.set(numStars);
+    this.currentRating.set(numStarsBounded);
     this.speakStars((cur, max) => `SET TO ${cur} STARS OUT OF ${max}`);
-    this.starsChanged.emit(numStars);
+    this.starsChanged.emit(numStarsBounded);
   }
 
   protected speakStars(
-    formatter: (current: number, max: number) => string = (cur, max) =>
-      `${cur} STARS OUT OF ${max}`,
+    formatter: (current: number, max: number) => string = (cur, max) => `${cur} STARS OUT OF ${max}`,
   ) {
     if (this.enableNarrate()) {
       if (this.narrateSvc == null) {
