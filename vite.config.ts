79bd2f3531fac77f8136c3787a911e6c4a4ac026
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2025-07-14 13:50:31 -0300

    migrate to analogjs

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    cacheDir: './node_modules/.vite',
+    build: {
+      outDir: './dist/./client',
+      reportCompressedSize: true,
+      target: ['es2020'],
+    },
+    plugins: [
+      analog({
+        ssr: false,
+        static: true,
+        prerender: {
+          routes: [],
+        },
+      }),
+
+      viteTsConfigPaths(),
+    ],
+    server: {
+      fs: {
+        allow: ['.'],
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
